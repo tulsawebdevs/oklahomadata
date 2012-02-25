b7a4c62f3879a718831ccb88838a1e2052a1feb7
Josh Mize <jgmize@gmail.com>
2012-02-25 15:46:06 -0600

    fixed UploadView.form_valid()

--- datafile/views.py
+++ datafile/views.py
@@ -1,7 +1,8 @@
-from django.views.generic.edit import *
+from django.views.generic.edit import FormView
 
 from datafile.forms import UploadForm
-import settings
+from django.conf import settings
+
 
 class UploadView(FormView):
     form_class = UploadForm
@@ -9,13 +10,9 @@ class UploadView(FormView):
     template_name = "datafile/upload.html"
 
     def form_valid(self, form):
-        def handle_uploaded_file(f):
-            destination = open("%s/%s" % (MEDIA_ROOT, f.name), 'wb+')
-            for chunk in f.chunks():
-                destination.write(chunk)
-            destination.close()
-
-        handle_uploaded_file(self.request.FILES['file'])
-
-        print(form.cleaned_data)
+        f = self.request.FILES['csv_file']
+        destination = open("%s/%s" % (settings.MEDIA_ROOT, f.name), 'wb+')
+        for chunk in f.chunks():
+            destination.write(chunk)
+        destination.close()
         return super(UploadView, self).form_valid(form)

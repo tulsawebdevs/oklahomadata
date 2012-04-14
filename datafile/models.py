7da85748345fbc0b3bd2d969f06b2ba8744a206c
Jeremy Satterfield <jsatt22@gmail.com>
2012-04-13 23:54:19 -0500

    add datafile model

--- datafile/models.py
+++ datafile/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+
+settings.DATAFILES_SUBDIR = getattr(settings, 'DATAFILES_SUBDIR',
+                                     'datafiles')
+FILETYPE_CHOICES = (
+    ('csv', 'CSV'),
+    ('json', 'JSON'),
+    ('xml', 'XML'))
+
+
+class DataFile(TimeStampedModel):
+    file = models.FileField(upload_to=settings.DATAFILES_SUBDIR)
+    filetype = models.CharField(max_length=20, choices=FILETYPE_CHOICES,
+                                null=True, blank=True)
+
+    def __unicode__(self):
+        return unicode(self.file.name)
--- datafile/views.py
+++ datafile/views.py
@@ -1,7 +1,8 @@
 from django.views.generic.edit import FormView
 
+from django.contrib import messages
+
 from datafile.forms import UploadForm
-from django.conf import settings
 
 
 class UploadView(FormView):
@@ -9,10 +10,12 @@ class UploadView(FormView):
     success_url = "/"
     template_name = "datafile/upload.html"
 
-    def form_valid(self, form):
-        f = self.request.FILES['csv_file']
-        destination = open("%s/%s" % (settings.MEDIA_ROOT, f.name), 'wb+')
-        for chunk in f.chunks():
-            destination.write(chunk)
-        destination.close()
-        return super(UploadView, self).form_valid(form)
+    def form_valid(self, form, *args, **kwargs):
+        form.save()
+        messages.success(self.request, 'File saved successfully')
+        return super(UploadView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request,
+                         'Failed to upload file. Please try again.')
+        return super(UploadView, self).form_invalid(form, *args, **kwargs)

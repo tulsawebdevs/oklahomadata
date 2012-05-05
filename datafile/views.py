21a9d033e1bad108b0c653542f146f1dfbf83ed0
Jeremy Satterfield <jsatt22@gmail.com>
2012-05-05 18:43:34 -0500

    add initial data file list, details and download functionality

--- datafile/views.py
+++ datafile/views.py
@@ -1,14 +1,50 @@
-from django.views.generic.edit import FormView
-
 from django.contrib import messages
+from django.http import Http404, HttpResponse
+from django.views.generic import DetailView, FormView, ListView, View
+from django.views.generic.detail import SingleObjectMixin
 
 from datafile.forms import UploadForm
+from datafile.models import DataFile
+
+
+class DataFileDetails(DetailView):
+    model = DataFile
+    template_name = 'datafile/file_details.html'
+
+
+class DataFileDownload(SingleObjectMixin, View):
+    model = DataFile
+    file_field = 'file'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        try:
+            self.file_obj = getattr(self.object, self.file_field)
+        except AttributeError:
+            raise Http404
+        else:
+            if self.object.filetype == 'json':
+                mimetype = 'application/json'
+            else:
+                assert self.object.filetype == 'csv'
+                mimetype = 'text/csv'
+            response =  HttpResponse(self.file_obj.file.read(),
+                                     content_type=mimetype)
+            response['Content-Disposition'] = 'filename=%s.%s' % (
+                self.file_obj.name.split('/')[-1], self.object.filetype)
+            return response
+
+
+class DataFilesList(ListView):
+    model = DataFile
+    template_name = 'datafile/file_list.html'
 
 
 class UploadView(FormView):
     form_class = UploadForm
-    success_url = "/"
-    template_name = "datafile/upload.html"
+    success_url = '/'
+    template_name = 'datafile/upload.html'
 
     def form_valid(self, form, *args, **kwargs):
         form.save()
--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import TemplateView
-from datafile.views import UploadView
+from datafile.views import (DataFileDetails, DataFileDownload, DataFilesList,
+                            UploadView)
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -17,6 +18,11 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name="home/index.html"),
         name='home'),
+    url(r'^datafiles/$', DataFilesList.as_view(), name='datafile_list'),
+    url(r'^datafiles/(?P<slug>[a-z0-9\-]+)/$', DataFileDetails.as_view(),
+        name='datafile_details'),
+    url(r'^datafiles/(?P<slug>[a-z0-9\-]+)/download/$',
+        DataFileDownload.as_view(), name='datafile_download'),
     url(r'^upload/$', UploadView.as_view(), name='upload_form'),
     (r'^boundary/', include('boundaryservice.urls')),
     (r'^boundary/$', include('boundary_demo.urls')),
